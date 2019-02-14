4e53e627ee2c6b0bfc7b638f2abb46255c2ab828
Sher Chowdhury <sher.chowdhury@gmail.com>
2019-02-14 10:46:19 +0000

    install working

--- image_build_scripts/03_install_wp_plugins.sh
+++ image_build_scripts/03_install_wp_plugins.sh
@@ -22,6 +22,7 @@ declare -a plugins=(
   wp-github-sync
   wpforms-lite
   easy-table-of-contents
+  coming-soon
   google-sitemap-generator
   wp-markdown    # needed by the github sync plugin
   # duplicate-post - dont need this if using github/jekyll
